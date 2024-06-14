492ae0f1ea158814a081f84a8036bca21d5d5e57
vintageGent <166114189+vintageGent@users.noreply.github.com>
2024-06-14 20:48:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+age = [19, 20, 21]
+print(age)
+print(fruits[2])
+print(age[1])
+print(fruits[-2])
+print("\n")
+#Append
+fruits.append("watermelon")
+print(fruits)
+age.append(22)
+print(age)
+
+#Insert
+fruits.insert(0, "pineapples")
+print(fruits)
+age.insert(-2, 18)
+print(age)
+print("\n")
+#remove
+fruits.remove("cherry")
+print(fruits)
+print("\n")
+#clear
+#fruits.clear()
+print(fruits)
+print("\n")
+#pop
+print(fruits.pop(0))
+
